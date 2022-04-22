94399f96136551e33f9d3d4c37bd8e3df05b4b77
Md Tauseef Akhtar <63928008+mdtauseef123@users.noreply.github.com>
2022-04-22 19:50:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function otpSent(){
+  window.alert("OTP has been sent to your email")
+}
+
+function regSuccess(){
+  window.alert("Your email has been registered successfully.")
+}
+
+function contFunc(){
+  window.alert("Your response has been submited successfully.")
+}
